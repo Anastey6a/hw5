7865106f8abab12d83677b9bc149c016a555d7d8
Anastey6a <61656958+Anastey6a@users.noreply.github.com>
2023-12-18 20:56:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDiv() {
+    var myDiv = document.getElementById('myDiv');
+    myDiv.style.display = 'block';
+}
+
+function hideDiv() {
+    var myDiv = document.getElementById('myDiv');
+    myDiv.style.display = 'none';
+}
